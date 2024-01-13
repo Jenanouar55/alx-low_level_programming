ba5d226220bad76e624652a0925e75e7f1fcc10c
Jenanouar55 <jenanouar55@gmail.com>
2024-01-13 03:14:44 +0100

    pushed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: number of commande-line arguments
+ * @argv: array of strings containing the command-line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
