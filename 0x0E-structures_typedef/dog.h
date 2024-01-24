ac010142d44028934e97fc3615e3ef6af8fdd63b
Jenanouar55 <jenanouar55@gmail.com>
2024-01-24 17:22:12 +0100

    pushed

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
