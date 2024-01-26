106926ae5578daf0e36a05b2254a6d3177eb637f
Jenanouar55 <jenanouar55@gmail.com>
2024-01-26 13:59:24 +0100

    pushed

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Print numbers using a separator
+ * @separator: The string separator
+ * @n: The quantity of numbers
+ * @...: The list of numbers
+ *
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *p;
+
+	va_start(valist, n);
+
+	i = 0;
+	while (i < n)
+	{
+		if (separator != NULL && i != 0)
+			printf("%s", separator);
+		p = va_arg(valist, char *);
+		printf("%s", (p == NULL) ? "(nil)" : p);
+	}
+	i++;
+	printf("\n");
+	va_end(valist);
+}
