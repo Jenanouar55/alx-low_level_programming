a2800a5e12e27b324b312edc56c5c6741a2162a9
Jenanouar55 <jenanouar55@gmail.com>
2024-01-26 15:01:05 +0100

    pushed

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *  * _strpbrk - Entry point
- *   * @s: input
- *    * @accept: input
- *     * Return: Always 0 (Success)
- *      */
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	int k;
+
 	while (*s)
 	{
 		for (k = 0; accept[k]; k++)
